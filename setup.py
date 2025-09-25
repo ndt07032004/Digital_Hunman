61cd65748098ed3d82c23c26733257b88a83676f
ndt07032004 <danhthai07032004ndt@gmail.com>
2025-09-25 23:05:28 +0700

    wed app compete

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name="Digital_Hunman", 
+    version="0.1.0",
+    author="Your Name",
+    author_email="",
+    package=find_packages(),
+    install_requires=[]
+    
+)
