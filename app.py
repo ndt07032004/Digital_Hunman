3379d10e99a1de5a8491d6e29ad59c83431036bd
ndt07032004 <danhthai07032004ndt@gmail.com>
2025-09-27 15:52:39 +0700

    hehe

--- app.py
+++ app.py
@@ -31,9 +31,6 @@ docsearch = PineconeVectorStore.from_existing_index(
     embedding=embeddings
 )
 
-
-
-
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
 chatModel = ChatOllama(model="gemma3:4b", base_url="http://localhost:11434")
